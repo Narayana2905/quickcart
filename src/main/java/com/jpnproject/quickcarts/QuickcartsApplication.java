19bf85ad2f7edec3d9f972c1ef12860de6dbb3cb
jpnarayana <jpnarayana2905@gmail.com>
2026-07-18 14:26:05 +0530

    commiting the local repository

---
+++ src/main/java/com/jpnproject/quickcarts/QuickcartsApplication.java
@@ -0,0 +1,13 @@
+package com.jpnproject.quickcarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickcartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickcartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpnproject/quickcarts/QuickcartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpnproject.quickcarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
